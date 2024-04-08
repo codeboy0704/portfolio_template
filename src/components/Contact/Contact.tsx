3a3fed0549e309b6357692425580d9a1de247e0e
Draigon07 <ian.flores2@utp.ac.pa>
2024-04-07 23:11:51 -0500

    final details done

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,15 +15,15 @@ export function Contact(){
       </header>
       <div className="contacts">
         <div>
-        <a href="mailto:Hello@vinayaksingh.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:Hello@vinayaksingh.com">Hello@vinayaksingh.com</a>
+        <a href="mailto:ianm2927@hotmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:ianm2927@hotmail.com">ianm2927@hotmail.com</a>
         </div>
         <div>
-        <a href="tel:+919630576848"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919630576848">(+91) 9630576848</a>
+        <a href="tel:+50760568927"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+50760568927">(+507) 60568927</a>
         </div>  
       </div>
-      <Form></Form>
+      {/* <Form></Form> */}
     </Container>
   )
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -11,9 +11,34 @@ export function Project() {
       <h2>My Projects</h2>
       <div className="projects">
 
-  
-
       <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://bambinos.netlify.app" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Bambinos -Restaurant V1</h3>
+              <p>
+              React-powered website, driven by seamless integration between frontend and backend. Leveraging Node.js and MongoDB, I've crafted a smooth experience to access data and services efficiently
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Mongo DB</li>
+                <li>Tailwind css</li>
+                <li>Node JS</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
@@ -26,7 +51,7 @@ export function Project() {
             <div className="body">
               <h3>Summarify</h3>
               <p>
-              Summarify is a tool developed to summarize articles based on a link with the help of artificial intelligence.
+                Summarify is a tool developed to summarize articles based on a link with the help of artificial intelligence.
               </p>
             </div>
             <footer>
@@ -53,13 +78,40 @@ export function Project() {
             <div className="body">
               <h3>Coffe Cafe- Cafe order Site</h3>
               <p>
-              The site was built with the aim of creating a responsive and visually appealing design for the client, using tools such as Tailwind CSS and React.
+                The site was built with the aim of creating a responsive and visually appealing design for the client, using tools such as Tailwind CSS and React.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
                 <li>Tailwind</li>
                 <li>React</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://v1--evogymtypescript.netlify.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Evogym-Typescript</h3>
+              <p>
+                The site was built with the aim of creating a revolutionary design integrating React and typescript
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Typescript</li>
+                <li>React</li>
+                <li>Css</li>
+                <li>Tailwind css</li>
               </ul>
             </footer>
           </div>
