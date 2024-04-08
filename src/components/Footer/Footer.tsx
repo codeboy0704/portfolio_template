f85177cd24a92c46c315e0c2ff7439e21ed1a4ba
Draigon07 <ian.flores2@utp.ac.pa>
2024-04-08 12:33:00 -0500

    changes on yarn

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,7 @@ import reactIcon from '../../assets/react-icon.svg'
 import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
-import telegram from '../../assets/telegram.svg'
 import instagramIcon from '../../assets/instagram.svg'
-// import discordIcon from '../../assets/discord.png'
 
 export function Footer() {
   return (
